5cb7a25bd87a4009f5e8e31bf24d8f637a1921e8
vivienl <vivienl@umich.edu>
2025-10-21 20:15:30 -0400

    removed extra files

--- .cpp/robot.c
+++
@@ -1,7 +0,0 @@
-#include <WiFi.h>
-#include <WebServer.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/queue.h>
-
-// consider using separate servers for G-code and CV position updates set with different priorities
